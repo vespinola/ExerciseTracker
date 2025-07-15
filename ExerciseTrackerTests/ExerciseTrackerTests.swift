1bad995cd1484a6de6d4ec15ae4b301f146fe563
Vladimir Espinola <vespinola@attentivemobile.com>
2025-07-14 23:57:14 -0300

    Initial Commit

---
+++ ExerciseTrackerTests/ExerciseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseTrackerTests.swift
+//  ExerciseTrackerTests
+//
+//  Created by Vladimir Espinola Lezcano on 2025-07-14.
+//
+
+import Testing
+@testable import ExerciseTracker
+
+struct ExerciseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
